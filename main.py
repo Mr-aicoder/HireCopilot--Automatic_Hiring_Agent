226ba7b4b282f7c6eafa50130d7b908ff294d902
Sachin Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2026-02-02 01:08:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os 
 import uuid 
 import utils.database as db 
-from agents.interview_question_agent import generate_interview_questions
+from agents.interview_question_agent  import generate_interview_questions
 from graphs.jd_graph import create_jd_graph
 from graphs.screening_graph import create_screening_graph
 from agents.summerizer_agent import summarizer_agent
