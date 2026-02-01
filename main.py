c0255d1f9693f67749b3a51177e7d32065f503ae
Sachin Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2026-02-02 01:08:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import uuid 
 import utils.database as db 
 from agents.interview_question_agent  import generate_interview_questions 
-from graphs.jd_graph import create_jd_graph
+from graphs.jd_graph import  create_jd_graph
 from graphs.screening_graph import create_screening_graph
 from agents.summerizer_agent import summarizer_agent
 
